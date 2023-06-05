bff7d0ffc4fe270ac72fdb93174d9938f1771a75
Lika Sobolenko <lika160892@yandex.ru>
2023-06-05 22:16:17 +0700

    made a pie chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Chart } from "react-google-charts";
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Sad", 4],
+  ["Happy", 2],
+  ["Anxiety", 1],
+  ["Annoyance", 2.5],
+  ["Excited", 3],
+  ["Confidence", 0.5],
+  ["Confusion", 0.7],
+  ["Guilt", 1],
+];
+export const options = {
+  title: "Feeling over day",
+};
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
